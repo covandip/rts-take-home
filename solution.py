c5d2b9a69e0aff053a44b02fab210119fa8447b7
Cameron <asdfa@asd>
2021-11-29 15:01:33 -0500

    added docstrings for the functions

--- solution.py
+++ solution.py
@@ -1,5 +1,15 @@
 class Solution:
+
     def aboveBelow(self, numbers: list, comparator: int) -> dict:
+        """
+        This function will take a list of ints and a int to compare them to and return
+        a dict with the number of ints above and below as values to the keys "above" and
+        "below".
+        :param numbers: A list of integers
+        :param comparator: An integer to compare the list values
+        :return: A dict with the keys "above" and "below" that map to the number of ints
+                 that are above and below the comparator int
+        """
         # validate arguments
         if not (isinstance(numbers, list) and all(isinstance(x, int) for x in numbers)):
             raise Exception('numbers must be a list of ints')
@@ -11,6 +21,12 @@ class Solution:
         return {'below': below, 'above': above}
 
     def stringRotation(self, rotationString: str, n: int) -> str:
+        """
+        This function will rotate a string to the right and place the overflow in the front.
+        :param rotationString: A string to be rotated.
+        :param n: A positive integer amount of space to rotate the string by
+        :return: Rotated string
+        """
         # validate arguments
         if not isinstance(rotationString, str):
             raise Exception('rotationString must be a string.')
